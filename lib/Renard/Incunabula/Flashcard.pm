6cc1271414c818a03de97614de35e33f2cf9e92a
Zakariyya Mughal <zaki.mughal@gmail.com>
2018-05-31 10:46:14 -0400

    Initial commit

---
+++ lib/Renard/Incunabula/Flashcard.pm
@@ -0,0 +1,12 @@
+use Renard::Incunabula::Common::Setup;
+package Renard::Incunabula::Flashcard;
+# ABSTRACT: Flashcard handling library
+
+use Moo;
+
+1;
+=head1 SEE ALSO
+
+L<Repository information|http://project-renard.github.io/doc/development/repo/p5-Renard-Incunabula-Flashcard/>
+
+=cut
